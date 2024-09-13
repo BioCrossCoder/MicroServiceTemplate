953dfe527114acca8c5c73cf0cda2fe3ddbb1cd0
BioCrossCoder <viscaria_dignics@163.com>
2024-09-13 10:56:01 +0800

    [feat] Add constants

---
+++ common/constants.go
@@ -0,0 +1,40 @@
+package common
+
+import (
+	"github.com/biocrosscoder/flex/typed/collections/arraylist"
+	"github.com/biocrosscoder/flex/typed/collections/dict"
+)
+
+const (
+	LanguageZHCN = "zh_cn"
+	LanguageENUS = "en_us"
+	User         = "user"
+	Department   = "department"
+	UserGroup    = "group"
+)
+
+var (
+	Languages    = arraylist.Of(LanguageZHCN, LanguageENUS)
+	UserTypeTag  = arraylist.Of(User, Department, UserGroup)
+	UserTypeCode = dict.Dict[string, int]{User: 0, Department: 1, UserGroup: 2}
+)
+
+const (
+	UserRole_NormalUser = 1 << iota
+	UserRole_AppAdmin
+	UserRole_SystemAdmin
+	UserRole_SuperAdmin = UserRole_AppAdmin | UserRole_SystemAdmin
+)
+
+const (
+	LogLevel_INFO = iota
+	LogLevel_WARN
+)
+
+const (
+	OpType_CREATE = iota
+	OpType_ADD
+	OpType_SET
+	OpType_DELETE
+	OpType_EDIT
+)
